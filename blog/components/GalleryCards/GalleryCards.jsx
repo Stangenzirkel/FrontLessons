da52a4b224ff4c8344c07a4d27f96078e512768b
Mikhail Buyanov <mi.buyanov@yandex.ru>
2022-11-09 22:16:22 +0300

    gallery route created

---
+++ blog/components/GalleryCards/GalleryCards.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const GalleryCards = () => {
+    return <p> GalleryCards </p>;
+}
---
+++ blog/pages/gallery/index.jsx
@@ -0,0 +1,9 @@
+import {GalleryForm} from "../../components/GalleryForm/GalleryForm";
+import {GalleryCards} from "../../components/GalleryCards/GalleryCards";
+
+export default function indexGallery() {
+    return <div>
+        <GalleryForm></GalleryForm>
+        <GalleryCards></GalleryCards>
+    </div>;
+}
