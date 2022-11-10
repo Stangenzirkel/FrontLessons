b43ec00ad468c39885b8eff208c9e6b27c8fdfc3
Mikhail Buyanov <mi.buyanov@yandex.ru>
2022-11-10 13:59:50 +0300

    form clear

--- blog/components/GalleryForm/GalleryForm.jsx
+++ blog/components/GalleryForm/GalleryForm.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const GalleryForm = ({onSubmit}) => {
-    return <form onSubmit={(e) => {onSubmit(e)}}>
+    return <form onSubmit={(e) => {onSubmit(e); e.target.url.value = ""; e.target.description.value = ""}}>
         <button type="submit"> Add </button>
         <input id="url" type="text" placeholder="url"/>
         <input id="description" type="text" placeholder="description"/>
